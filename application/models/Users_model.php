547fc0704be58101173f88c0bc2a3b2323bde961
Alvaro Glz <alvarogonzalez0509@gmail.com>
2024-04-26 18:41:48 -0600

    Funcionando el read del data en users a la hora de cambiar groups por roles

--- application/models/Users_model.php
+++ application/models/Users_model.php
@@ -5,10 +5,10 @@ class Users_model extends CI_Model {
     
     public function getDatausers($id = null)
     {
-        $this->datatables->select('users.id, username, first_name, last_name, email, FROM_UNIXTIME(created_on) as created_on, last_login, active, groups.name as level');
+        $this->datatables->select('users.id, username, first_name, last_name, email, FROM_UNIXTIME(created_on) as created_on, last_login, active, roles.name as level');
         $this->datatables->from('users_groups');
         $this->datatables->join('users', 'users_groups.user_id=users.id');
-        $this->datatables->join('groups', 'users_groups.group_id=groups.id');
+        $this->datatables->join('roles', 'users_groups.group_id=roles.id');
         if($id !== null){
             $this->datatables->where('users.id !=', $id);
         }
